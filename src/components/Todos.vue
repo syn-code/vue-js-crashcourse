57219d86a299e7332b3daaacab005a8a24f627e7
James <james@pop-os.localdomain>
2020-01-26 00:41:31 +0000

    first commit with vue js

---
+++ src/components/Todos.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import TodoItem from '../components/TodoItem'
+
+export default {
+    //compnent name: goes here
+    name: "Todos",
+    components:{
+        TodoItem
+    },
+    /*
+        Accepting the props that are being passed into the component in                      App.vue. These will be passed in with v-bind
+    */
+    props: ["todos"]
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
