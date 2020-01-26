a9d2d6f99bfa91bef6b34856fb982142409a2487
James <james@pop-os.localdomain>
2020-01-26 01:29:31 +0000

    updates to todo app

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
-        <p>{{ todo.title }}</p>
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{ todo.title }}
+            <button class="del" @click="$emit('del-todo', todo.id)">x</button>
+        </p>
     </div>
 </template>
 
@@ -8,7 +12,14 @@
 export default {
     name: "TodoItem",
     //this binds each 'key' from within the v-for loop within Todos
-    props: ["todo"]
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            this.todo.completed = !this.todo.completed;
+        },
+       
+    }
+    
 
 }
 </script>
